dfc932fb8ccdce856c836e7ee5281117b8a534fe
Mindasteris <mminde41@gmail.com>
2023-03-07 09:57:03 +0200

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,13 +10,13 @@ const About = () => {
         </div>
 
         {/* Flex Container */}
-       <div className='flex flex-col lg:flex-row items-center'>
-            <div>
+       <div className='flex flex-col lg:flex-row justify-center items-center'>
+            <div className='w-full lg:w-1/2'>
                 <p className='p-4 mt-4 lg:p-0 w-full max-w-[700px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat officia modi doloribus quidem beatae repudiandae officiis, aperiam, consequatur veritatis rerum provident. Repellendus recusandae architecto id quaerat deleniti, quisquam debitis rem? Ab accusamus eveniet maxime, obcaecati similique in corrupti, illo cupiditate rerum, quis itaque voluptates facilis aspernatur autem officia quo impedit quibusdam nulla quasi! Delectus aliquid totam beatae aliquam, placeat, suscipit ex nesciunt quisquam autem est dolorem? Magni, magnam iusto enim numquam perspiciatis hic officia, tempore atque cumque ut, voluptatum ab quasi minima aliquam corrupti velit commodi ipsam illum voluptatem. Laudantium asperiores voluptatem quod assumenda temporibus? Adipisci aliquam fuga laborum exercitationem.</p>
             </div>
 
-            <div>
-                <img src={dog4} alt="" />
+            <div className='w-full lg:w-1/2'>
+                <img src={dog4} alt="dog-4" className='mx-auto' />
             </div>
        </div>
     </section>
